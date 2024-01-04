1c02714c11dba34c0e640e435cb006398253e20e
MarkvG <vgmark@icloud.com>
2024-01-04 22:15:04 +0100

    Add Controller

---
+++ GitProjektWHS/VersionsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GitProjektWHS
+{
+    public class VersionsContext : DbContext 
+    {
+        public VersionsContext(DbContextOptions<VersionsContext> options) : base(options)
+        {
+                
+        }
+        public  DbSet<VersionsDatei> Datei { get; set; }
+        public DbSet<VersionsObjekt> Versions { get; set; }
+    }
+}

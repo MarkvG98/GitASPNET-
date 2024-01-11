e874f4ac1ed96beb76a46fa276839a9bed5a9854
MarkvG <vgmark@icloud.com>
2024-01-11 21:15:01 +0100

    ReadAllFiles im Directroy. Anschließend Update oder Create des Objektes

--- GitProjektWHS/VersionsObjekt.cs
+++ GitProjektWHS/VersionsObjekt.cs
@@ -4,6 +4,7 @@
     {
         public int ID { get; set; }
         public int DateiID { get; set; }
+        public int Version { get; set; }
         public string? Tag { get; set; }
         public string? Inhalt { get; set; }
         public string? Filename { get; set; }
